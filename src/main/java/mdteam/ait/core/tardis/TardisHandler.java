7bff287b365338c4b69b4775b6562307197bc990
Jayson Hartmann <jaysonjson@web.de>
2023-10-22 21:07:44 +0100

    tardis data

---
+++ src/main/java/mdteam/ait/core/tardis/TardisHandler.java
@@ -0,0 +1,53 @@
+package mdteam.ait.core.tardis;
+
+import mdteam.ait.AITMod;
+import net.minecraft.util.WorldSavePath;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import static net.minecraft.datafixer.fix.BlockEntitySignTextStrictJsonFix.GSON;
+
+public class TardisHandler {
+
+    public Map<UUID, Tardis> tardisses = new HashMap<>();
+
+    @Nullable
+    Tardis getTardis(UUID uuid) {
+        if (!tardisses.containsKey(uuid)) {
+            loadTardis(uuid);
+        }
+        return tardisses.get(uuid);
+    }
+
+    public void loadTardis(UUID uuid) {
+        if(tardisses.containsKey(uuid)) return;
+        File file = new File(AITMod.mcServer.getSavePath(WorldSavePath.ROOT) + "ait/" + uuid + ".json");
+        if(!file.exists()) return;
+        try {
+            String fileContent = new String(Files.readAllBytes(file.toPath()));
+            tardisses.put(uuid, GSON.fromJson(fileContent, Tardis.class));
+        } catch (Exception e) {
+            AITMod.LOGGER.warn("Tardis " + uuid + " does not exist");
+        }
+    }
+
+    public void saveTardis(Tardis tardis) {
+        File file = new File(AITMod.mcServer.getSavePath(WorldSavePath.ROOT) + "ait/" + tardis.getUuid() + ".json");
+        if(!file.getParentFile().exists()) file.getParentFile().mkdirs();
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file.getPath()));
+            writer.write(GSON.toJson(tardis));
+            writer.close();
+        } catch (Exception e) {
+
+        }
+    }
+
+}
