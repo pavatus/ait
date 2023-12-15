6606de43f74649c24bcceb892287fa72eeb50aad
Duzo <james.hall01@outlook.com>
2023-12-15 21:52:43 +0000

    plsplspls

--- src/main/java/mdteam/ait/client/renderers/exteriors/ExteriorEnum.java
+++ src/main/java/mdteam/ait/client/renderers/exteriors/ExteriorEnum.java
@@ -14,6 +14,7 @@ import net.fabricmc.api.Environment;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 
+@Deprecated // try to avoid using this until i finish exterior-rewrite plsplspls
 public enum ExteriorEnum {
     CAPSULE() {
         @Override
