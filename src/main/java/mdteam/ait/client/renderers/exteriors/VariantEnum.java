47577dab4e95d48d7efa0e6816e035de02655f68
Loqor <65447472+Loqor@users.noreply.github.com>
2023-12-16 14:54:10 -0600

    updated all texture variants and changed their names from BONE/LAVA to FIRE/SOUL

--- src/main/java/mdteam/ait/client/renderers/exteriors/VariantEnum.java
+++ src/main/java/mdteam/ait/client/renderers/exteriors/VariantEnum.java
@@ -3,6 +3,6 @@ package mdteam.ait.client.renderers.exteriors;
 public enum VariantEnum {
     //@TODO finish this system so both the door and exteriors use it
     DEFAULT,
-    BONE,
-    LAVA
+    SOUL,
+    FIRE
 }
