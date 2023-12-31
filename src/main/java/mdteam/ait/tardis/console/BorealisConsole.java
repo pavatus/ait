e4e75bd69976ba7861f117211b72b6a73479f853
Loqor <65447472+Loqor@users.noreply.github.com>
2023-12-30 23:34:58 -0600

    console positions for the door lock and hads have been changed.

--- src/main/java/mdteam/ait/tardis/console/BorealisConsole.java
+++ src/main/java/mdteam/ait/tardis/console/BorealisConsole.java
@@ -18,7 +18,7 @@ public class BorealisConsole extends ConsoleSchema {
             new ControlTypes(new HandBrakeControl(), EntityDimensions.changing(0.175f, 0.175f), new Vector3f(-0.5750000085681677f, 0.4749999772757292f, -0.5750000234693289f)), //1
             new ControlTypes(new ThrottleControl(), EntityDimensions.changing(0.175f, 0.175f), new Vector3f(-0.8000000156462193f, 0.4749999772757292f, -0.20000000298023224f)), //2
             new ControlTypes(new DoorControl(), EntityDimensions.changing(0.175f, 0.175f), new Vector3f(0.0f, 0.4749999772757292f, 0.7750000078231096f)), //3
-            new ControlTypes(new DoorLockControl(), EntityDimensions.changing(0.175f, 0.175f), new Vector3f(0.0f, 0.9749999772757292f, 0.7750000078231096f)),
+            new ControlTypes(new DoorLockControl(), EntityDimensions.changing(0.087499976f, 0.087499976f), new Vector3f(0.0f, 0.6249998994171619f, 0.6003906223922968f)),
             new ControlTypes(new XControl(), EntityDimensions.changing(0.1f, 0.1f), new Vector3f(0.12500001676380634f, 0.6499999798834324f, -0.5250000078231096f)), //4
             new ControlTypes(new YControl(), EntityDimensions.changing(0.1f, 0.1f), new Vector3f(0.0f, 0.6499999798834324f, -0.5249999929219484f)), //5
             new ControlTypes(new ZControl(), EntityDimensions.changing(0.1f, 0.1f), new Vector3f(-0.12499999813735485f, 0.6499999798834324f, -0.5250000040978193f)), //6
@@ -31,7 +31,7 @@ public class BorealisConsole extends ConsoleSchema {
             new ControlTypes(new AutoPilotControl(), EntityDimensions.changing(0.1f, 0.1f), new Vector3f(-0.421875f, 0.6750001907348633f, 0.25f)), //13
             new ControlTypes(new HailMaryControl(), EntityDimensions.changing(0.074999996f, 0.074999996f), new Vector3f(-0.5468749962747097f, 0.6749999076128006f, -0.17421874962747097f)), //2
             new ControlTypes(new FastReturnControl(), EntityDimensions.changing(0.075f, 0.075f), new Vector3f(-0.4000000134110451f, 0.674999987706542f, -0.37500001303851604f)), //idk
-            new ControlTypes(new HADSControl(), EntityDimensions.changing(0.074999996f, 0.1f), new Vector3f(0.516015631146729f, 0.5750001845881343f, 0.40332030411809683f)),
+            new ControlTypes(new HADSControl(), EntityDimensions.changing(0.074999996f, 0.099999994f), new Vector3f(0.6406250018626451f, 0.625000286847353f, -0.14765625819563866f)),
             new ControlTypes(new AntiGravsControl(), EntityDimensions.changing(0.1f, 0.1f), new Vector3f(0.8750000037252903f, 0.5249999780207872f, 0.15156249701976776f)), //14
             new ControlTypes(new RefuelerControl(), EntityDimensions.changing(0.1f, 0.1f), new Vector3f(0.6593750026077032f, 0.5499999783933163f, -0.3812500014901161f)), //15
     };
