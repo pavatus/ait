c97d3247451f3fa241da79c1a9142c1ad78293eb
Duzo <james.hall01@outlook.com>
2024-01-04 19:02:02 +0000

    fix dupe check
    Signed-off-by: Duzo <james.hall01@outlook.com>

--- src/main/java/mdteam/ait/tardis/handler/InteriorChangingHandler.java
+++ src/main/java/mdteam/ait/tardis/handler/InteriorChangingHandler.java
@@ -123,7 +123,6 @@ public class InteriorChangingHandler extends TardisLink {
             tardis().getHandlers().getAlarms().disable();
             return;
         }
-        if (!isGenerating()) return;
 
         if (!isInteriorEmpty()) {
             warnPlayers();
