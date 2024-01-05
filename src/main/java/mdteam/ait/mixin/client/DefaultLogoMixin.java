d9683c90b099cc3df2d352a24ef798d31a8b7f9d
Duzo <james.hall01@outlook.com>
2024-01-05 13:55:42 +0000

    fix issue #119
    Signed-off-by: Duzo <james.hall01@outlook.com>

--- src/main/java/mdteam/ait/mixin/client/DefaultLogoMixin.java
+++ src/main/java/mdteam/ait/mixin/client/DefaultLogoMixin.java
@@ -10,6 +10,7 @@ import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = LogoDrawer.class, priority = 1001)
 public class DefaultLogoMixin {
@@ -23,11 +24,15 @@ public class DefaultLogoMixin {
 
     //This is setting the logo to BOTH the logo itself and the edition, as they're both in the same texture.
     //Also, while on the topic, I had to make a new texture since the old one only worked for older versions of Minecraft, as 1.19+ now uses a scaled and blurred version of the logo for the title screen.
-    @Redirect(method = "Lnet/minecraft/client/gui/LogoDrawer;draw(Lnet/minecraft/client/gui/DrawContext;IFI)V",
+    @Redirect(method = "draw(Lnet/minecraft/client/gui/DrawContext;IFI)V",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lnet/minecraft/util/Identifier;IIFFIIII)V", ordinal = 0))
     private void injectedLogo(DrawContext instance, Identifier texture, int x, int y, float u, float v, int width, int height, int textureWidth, int textureHeight) {
-        if (!AITMod.AIT_CONFIG.CUSTOM_MENU()) return;
+        if (!AITMod.AIT_CONFIG.CUSTOM_MENU()) {
+            instance.drawTexture(texture, x, y, u, v, width, height, textureWidth, textureHeight);
+            return;
+        }
 
+        assert this.client.currentScreen != null;
         int screenWidth = this.client.currentScreen.width;
         int i = screenWidth / 2 - 128;
         instance.drawTexture(AIT_LOGO, i, y, 0.0f, 0.0f, 256, 64, 256, 64);
@@ -37,7 +42,10 @@ public class DefaultLogoMixin {
     @Redirect(method = "Lnet/minecraft/client/gui/LogoDrawer;draw(Lnet/minecraft/client/gui/DrawContext;IFI)V",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lnet/minecraft/util/Identifier;IIFFIIII)V", ordinal = 1))
     private void injectedEdition(DrawContext instance, Identifier texture, int x, int y, float u, float v, int width, int height, int textureWidth, int textureHeight) {
-        if (!AITMod.AIT_CONFIG.CUSTOM_MENU()) return;
+        if (!AITMod.AIT_CONFIG.CUSTOM_MENU()) {
+            instance.drawTexture(texture, x, y, u, v, width, height, textureWidth, textureHeight);
+            return;
+        }
 
         int screenWidth = this.client.currentScreen.width;
         int i = screenWidth / 2 - 128;
