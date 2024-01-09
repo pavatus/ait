e0c3124068ab602fd551f12e5153b559ad69e16f
Duzo <james.hall01@outlook.com>
2024-01-09 01:52:07 +0000

    added bad regen code

--- src/main/java/mdteam/ait/compat/regen/RegenHandler.java
+++ src/main/java/mdteam/ait/compat/regen/RegenHandler.java
@@ -62,6 +62,14 @@ public class RegenHandler implements Acting {
 
     @Override
     public void onRegenTick(IRegen iRegen) {
+        LivingEntity livingEntity = iRegen.getLiving();
+        World world = livingEntity.getWorld();
+        if (world.isClient()) return;
+
+        findTardis(livingEntity).ifPresent(tardis -> {
+            if (tardis.getTravel().getState() == TardisTravel.State.LANDED)
+                forceTakeOff(tardis);
+        });
     }
 
     @Override
