5285ff6058e72c6777fc55efb7cfc13964b33b3a
Duzo <james.hall01@outlook.com>
2024-01-30 16:41:55 +0000

    Change sculk click sounds

--- src/main/java/mdteam/ait/tardis/control/impl/CloakControl.java
+++ src/main/java/mdteam/ait/tardis/control/impl/CloakControl.java
@@ -1,9 +1,12 @@
 package mdteam.ait.tardis.control.impl;
 
+import mdteam.ait.core.AITSounds;
 import mdteam.ait.tardis.Tardis;
 import mdteam.ait.tardis.control.Control;
+import mdteam.ait.tardis.data.CloakData;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 
@@ -16,13 +19,21 @@ public class CloakControl extends Control {
     @Override
     public boolean runServer(Tardis tardis, ServerPlayerEntity player, ServerWorld world) {
 
-        tardis.getHandlers().getCloak().toggle();
+        CloakData cloak = tardis.getHandlers().getCloak();
+
+        cloak.toggle();
+
+        if (cloak.isEnabled()) {
+            world.playSound(null, player.getBlockPos(), SoundEvents.BLOCK_SCULK_SENSOR_CLICKING, SoundCategory.BLOCKS, 1.0F, 1.0F);
+        } else {
+            world.playSound(null, player.getBlockPos(), SoundEvents.BLOCK_SCULK_SENSOR_CLICKING_STOP, SoundCategory.BLOCKS, 1.0F, 1.0F);
+        }
 
         return true;
     }
 
     @Override
     public SoundEvent getSound() {
-        return SoundEvents.BLOCK_SCULK_SENSOR_CLICKING;
+        return SoundEvents.INTENTIONALLY_EMPTY;
     }
 }
