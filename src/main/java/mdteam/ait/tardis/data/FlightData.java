c64954eef8c9186a9a46d2c6170749bfec3d9d34
Duzo <james.hall01@outlook.com>
2024-02-01 23:50:07 +0000

    Moved TargetTicks over to PropertiesHandler

--- src/main/java/mdteam/ait/tardis/data/FlightData.java
+++ src/main/java/mdteam/ait/tardis/data/FlightData.java
@@ -12,7 +12,7 @@ import net.minecraft.util.math.MathHelper;
 
 public class FlightData extends TardisLink {
     private static final String FLIGHT_TICKS_KEY = "flight_ticks";
-    private int targetTicks = 0;
+    private static final String TARGET_TICKS_KEY = "target_ticks";
 
     public FlightData(Tardis tardiz) {
         super(tardiz, "flight");
@@ -24,14 +24,14 @@ public class FlightData extends TardisLink {
             if (!tardis.equals(this.getTardis().get())) return;
 
             this.setFlightTicks(0);
-            targetTicks = 0;
+            this.setTargetTicks(0);
         }));
         TardisEvents.DEMAT.register((tardis -> {
             if (this.getTardis().isEmpty()) return false;
             if (!tardis.equals(this.getTardis().get())) return false;
 
             this.setFlightTicks(0);
-            targetTicks = FlightUtil.getFlightDuration(tardis.getTravel().getPosition(), tardis.getTravel().getDestination());
+            this.setTargetTicks(FlightUtil.getFlightDuration(tardis.getTravel().getPosition(), tardis.getTravel().getDestination()));
 
             return false;
         }));
@@ -44,17 +44,17 @@ public class FlightData extends TardisLink {
 
     private boolean isFlightTicking() {
         if(getTardis().isEmpty()) return false;
-        return this.getTardis().get().getTravel().getState() == TardisTravel.State.FLIGHT && this.targetTicks != 0;
+        return this.getTardis().get().getTravel().getState() == TardisTravel.State.FLIGHT && this.getTargetTicks() != 0;
     }
 
     public boolean hasFinishedFlight() {
         if(getTardis().isEmpty()) return false;
-        return this.getFlightTicks() >= targetTicks || targetTicks == 0 || getTardis().get().getTravel().isCrashing();
+        return this.getFlightTicks() >= this.getTargetTicks() || this.getTargetTicks() == 0 || getTardis().get().getTravel().isCrashing();
     }
     private void onFlightFinished() {
         if(getTardis().isEmpty()) return;
         this.setFlightTicks(0);
-        this.targetTicks = 0;
+        this.setTargetTicks(0);
         FlightUtil.playSoundAtConsole(getTardis().get(), SoundEvents.BLOCK_BEACON_POWER_SELECT); // temp sound
 
         if (shouldAutoLand()) {
@@ -67,24 +67,24 @@ public class FlightData extends TardisLink {
     }
 
     public void increaseFlightTime(int ticks) {
-        targetTicks = targetTicks + ticks;
+        this.setTargetTicks(this.getTargetTicks() + ticks);
     }
 
     public int getDurationAsPercentage() {
         if(getTardis().isEmpty()) return 0;
-        if (this.targetTicks == 0 || this.getFlightTicks() == 0) {
+        if (this.getTargetTicks() == 0 || this.getFlightTicks() == 0) {
             if (this.getTardis().get().getTravel().getState() == TardisTravel.State.DEMAT) return 0;
             // if (this.tardis().getTravel().getState() == TardisTravel.State.MAT) return 100;
             return 100;
         }
 
-        return FlightUtil.getDurationAsPercentage(this.getFlightTicks(), this.targetTicks);
+        return FlightUtil.getDurationAsPercentage(this.getFlightTicks(), this.getTargetTicks());
     }
 
     public void recalculate() {
         if(getTardis().isEmpty()) return;
-        this.targetTicks = FlightUtil.getFlightDuration(getTardis().get().position(), getTardis().get().destination());
-        this.setFlightTicks(this.isInFlight() ? MathHelper.clamp(this.getFlightTicks(), 0, this.targetTicks) : 0);
+        this.setTargetTicks(FlightUtil.getFlightDuration(getTardis().get().position(), getTardis().get().destination()));
+        this.setFlightTicks(this.isInFlight() ? MathHelper.clamp(this.getFlightTicks(), 0, this.getTargetTicks()) : 0);
     }
 
     public int getFlightTicks() {
@@ -102,16 +102,31 @@ public class FlightData extends TardisLink {
        PropertiesHandler.set(this.getTardis().get(), FLIGHT_TICKS_KEY, ticks);
     }
 
+    public int getTargetTicks() {
+        if (this.getTardis().isEmpty()) return 0;
+
+        if (!this.getTardis().get().getHandlers().getProperties().getData().containsKey(TARGET_TICKS_KEY)) {
+            this.setTargetTicks(0);
+        }
+
+        return PropertiesHandler.getInt(this.getTardis().get().getHandlers().getProperties(), TARGET_TICKS_KEY);
+    }
+    private void setTargetTicks(int ticks) {
+        if (this.getTardis().isEmpty()) return;
+
+        PropertiesHandler.set(this.getTardis().get(), TARGET_TICKS_KEY, ticks);
+    }
+
     @Override
     public void tick(MinecraftServer server) {
         super.tick(server);
         if(getTardis().isEmpty()) return;
 
-        if ((this.targetTicks > 0 || this.getFlightTicks() > 0) && this.getTardis().get().getTravel().getState() == TardisTravel.State.LANDED) {
+        if ((this.getTargetTicks() > 0 || this.getFlightTicks() > 0) && this.getTardis().get().getTravel().getState() == TardisTravel.State.LANDED) {
             this.recalculate();
         }
 
-        if (this.isInFlight() && !this.getTardis().get().getTravel().isCrashing() && !(this.getFlightTicks() >= this.targetTicks) && this.targetTicks == 0) {
+        if (this.isInFlight() && !this.getTardis().get().getTravel().isCrashing() && !(this.getFlightTicks() >= this.getTargetTicks()) && this.getTargetTicks() == 0) {
             this.recalculate();
         }
 
