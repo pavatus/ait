9e80854f5022d85acb152e9a474b26845a9c3102
Duzo <james.hall01@outlook.com>
2024-02-04 21:30:27 +0000

    Made MonitorBlockEntity extend LinkableBlockEntity

--- src/main/java/mdteam/ait/core/blockentities/MonitorBlockEntity.java
+++ src/main/java/mdteam/ait/core/blockentities/MonitorBlockEntity.java
@@ -3,6 +3,7 @@ package mdteam.ait.core.blockentities;
 import mdteam.ait.AITMod;
 import mdteam.ait.core.AITBlockEntityTypes;
 import mdteam.ait.tardis.Tardis;
+import mdteam.ait.tardis.link.LinkableBlockEntity;
 import mdteam.ait.tardis.util.AbsoluteBlockPos;
 import mdteam.ait.tardis.util.TardisUtil;
 import net.minecraft.block.BlockState;
@@ -14,54 +15,33 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import static mdteam.ait.tardis.util.TardisUtil.findTardisByInterior;
 import static mdteam.ait.tardis.util.TardisUtil.findTardisByPosition;
 
-public class MonitorBlockEntity extends BlockEntity {
+public class MonitorBlockEntity extends LinkableBlockEntity {
 
-    public UUID tardisid;
 
     public MonitorBlockEntity(BlockPos pos, BlockState state) {
         super(AITBlockEntityTypes.MONITOR_BLOCK_ENTITY_TYPE, pos, state);
     }
 
-    @Override
-    protected void writeNbt(NbtCompound nbt) {
-        super.writeNbt(nbt);
-        if(this.getTardisID() != null)
-            nbt.putUuid("tardisid", this.getTardisID());
-    }
-
-    @Override
-    public void readNbt(NbtCompound nbt) {
-        if(nbt.contains("tardisid"))
-            this.setTardisID(nbt.getUuid("tardisid"));
-        super.readNbt(nbt);
-    }
-
     public void useOn(World world, boolean sneaking, PlayerEntity player) {
-        if(world.isClient() || this.getTardisID() == null) return;
-        AITMod.openScreen((ServerPlayerEntity) player, 0, this.getTardisID()); // we can cast because we know its on server :p
+        if (world.isClient() || this.getTardis().isEmpty()) return;
+        AITMod.openScreen((ServerPlayerEntity) player, 0, this.getTardis().get().getUuid()); // we can cast because we know its on server :p
     }
 
-    public void setTardisID(UUID id) {
-        this.tardisid = id;
-        markDirty();
-    }
 
-    public UUID getTardisID() {
-        if (tardisid == null) findTardisFromPosition();
-
-        return tardisid;
-    }
-    private void findTardisFromPosition() { // should only be used if tardisId is null so we can hopefully refind the tardis
-        Tardis found = findTardisByInterior(this.getPos(), !this.getWorld().isClient());
-
-        if (found == null) return;
-
-        this.tardisid = found.getUuid();
-        markDirty();
+    @Override
+    public Optional<Tardis> getTardis() {
+        if(this.tardisId == null && this.hasWorld()) {
+            assert this.getWorld() != null;
+            Tardis found = findTardisByInterior(pos, !this.getWorld().isClient());
+            if (found != null)
+                this.setTardis(found);
+        }
+        return super.getTardis();
     }
 }
