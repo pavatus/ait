9d017cec4ad1b36c5f8474605aa13e6d7aa4dad5
Loqor <65447472+Loqor@users.noreply.github.com>
2024-02-28 01:15:26 -0600

    Actually made it work now for the scanner sonic to be in the slot to make it work

--- src/main/java/mdteam/ait/client/renderers/entities/ControlEntityRenderer.java
+++ src/main/java/mdteam/ait/client/renderers/entities/ControlEntityRenderer.java
@@ -2,41 +2,30 @@ package mdteam.ait.client.renderers.entities;
 
 import mdteam.ait.AITMod;
 import mdteam.ait.client.models.consoles.ControlModel;
-import mdteam.ait.core.AITItems;
 import mdteam.ait.core.entities.ConsoleControlEntity;
 import mdteam.ait.core.item.SonicItem;
-import mdteam.ait.core.util.AITConfig;
-import mdteam.ait.core.util.AITConfigModel;
-import mdteam.ait.tardis.Tardis;
 import mdteam.ait.tardis.data.SonicHandler;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
-import net.minecraft.client.render.LightmapTextureManager;
 import net.minecraft.client.render.OverlayTexture;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.LivingEntityRenderer;
-import net.minecraft.client.render.entity.PlayerEntityRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.text.OrderedText;
-import net.minecraft.text.Style;
 import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.RotationAxis;
 import org.joml.Matrix4f;
-
-import static mdteam.ait.AITMod.AIT_CONFIG;
 
 @Environment(value = EnvType.CLIENT)
 public class ControlEntityRenderer
@@ -79,7 +68,7 @@ public class ControlEntityRenderer
         }
         matrices.pop();
         if (hitresult != null) {
-            boolean isPlayerHoldingScanningSonic = isPlayerHoldingScanningSonic(entity);
+            boolean isPlayerHoldingScanningSonic = isScanningSonicInConsole(entity);
             if (isPlayerHoldingScanningSonic) {
                 if (entity.isPartOfSequence()) {
                     matrices.push();
@@ -111,28 +100,28 @@ public class ControlEntityRenderer
                 NbtCompound nbt = sonic.getOrCreateNbt();
                 if (hitResult.getType() == HitResult.Type.ENTITY) {
                     Entity hitEntity = ((EntityHitResult) hitResult).getEntity();
-                    return hitEntity != null && hitEntity.equals(entity) && nbt.contains(SonicItem.MODE_KEY) && nbt.getInt(SonicItem.MODE_KEY) == 3;
+                    return hitEntity != null && hitEntity.equals(entity) && (nbt.getInt(SonicItem.PREV_MODE_KEY) == 3 || nbt.getInt(SonicItem.MODE_KEY) == 3);
                 }
             } else if (player.getOffHandStack().getItem() instanceof SonicItem) {
                 ItemStack sonic = player.getOffHandStack();
                 NbtCompound nbt = sonic.getOrCreateNbt();
                 if (hitResult.getType() == HitResult.Type.ENTITY) {
                     Entity hitEntity = ((EntityHitResult) hitResult).getEntity();
-                    return hitEntity != null && hitEntity.equals(entity) && nbt.contains(SonicItem.MODE_KEY) && nbt.getInt(SonicItem.MODE_KEY) == 3;
+                    return hitEntity != null && hitEntity.equals(entity) && (nbt.getInt(SonicItem.PREV_MODE_KEY) == 3 || nbt.getInt(SonicItem.MODE_KEY) == 3);
                 }
             }
         }
         return false;
     }
 
-    public static boolean isPlayerHoldingScanningSonic(ConsoleControlEntity entity) {
+    public static boolean isScanningSonicInConsole(ConsoleControlEntity entity) {
         if (entity.getWorld() == null || !entity.getWorld().isClient())
             return false;
         if(entity.getTardis() == null) return false;
-        if(entity.getTardis().getHandlers().getSonic().hasSonic(SonicHandler.HAS_CONSOLE_SONIC)) return false;
+        if(!entity.getTardis().getHandlers().getSonic().hasSonic(SonicHandler.HAS_CONSOLE_SONIC)) return false;
         ItemStack sonic = entity.getTardis().getHandlers().getSonic().get(SonicHandler.HAS_CONSOLE_SONIC);
             NbtCompound nbt = sonic.getOrCreateNbt();
-            return nbt.contains(SonicItem.MODE_KEY) && nbt.getInt(SonicItem.MODE_KEY) == 3;
+            return nbt.contains(SonicItem.MODE_KEY) && (nbt.getInt(SonicItem.PREV_MODE_KEY) == 3 || nbt.getInt(SonicItem.MODE_KEY) == 3);
     }
 
     @Override
