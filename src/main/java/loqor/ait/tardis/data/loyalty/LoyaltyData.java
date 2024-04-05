f65bf69248839a1e6d7c806467efed7b2dc07760
DrTheodor <DrTheodor@users.noreply.github.com>
2024-04-05 19:48:45 +0300

    refactor LoyaltyData class, add testing for loyalty type enum, change Loyalty to be a record containing loyalty value (the number) and type (the enum)

--- src/main/java/loqor/ait/tardis/data/loyalty/LoyaltyData.java
+++ src/main/java/loqor/ait/tardis/data/loyalty/LoyaltyData.java
@@ -5,63 +5,51 @@ import loqor.ait.tardis.data.TardisLink;
 import net.minecraft.server.network.ServerPlayerEntity;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
+import java.util.function.Function;
 
 public class LoyaltyData extends TardisLink {
-	private final HashMap<UUID, Loyalty> data;
+    private final Map<UUID, Loyalty> data;
 
-	public LoyaltyData(Tardis tardisId, HashMap<UUID, Loyalty> data) {
-		super(tardisId, "loyalty");
-		this.data = data;
-	}
+    public LoyaltyData(Tardis tardis, HashMap<UUID, Loyalty> data) {
+        super(tardis, "loyalty");
+        this.data = data;
+    }
 
 	public LoyaltyData(Tardis tardis) {
 		this(tardis, new HashMap<>());
 	}
 
-	public HashMap<UUID, Loyalty> data() {
-		return this.data;
-	}
+    public Map<UUID, Loyalty> data() {
+        return this.data;
+    }
 
-	public void addPlayer(ServerPlayerEntity player) {
-		this.setLoyalty(player, Loyalty.NEUTRAL);
-	}
+    public void add(ServerPlayerEntity player) {
+        this.set(player, new Loyalty(Loyalty.Type.NEUTRAL));
+    }
 
-	public void subtractLoyaltyLevel(ServerPlayerEntity player, int loyaltyValue) {
-		int playerLevel = this.get(player).level;
-		int newLevel = Math.min(Math.max(playerLevel - loyaltyValue, 0), Loyalty.OWNER.level);
-		this.setLoyalty(player, this.levelToLoyaltyValue(newLevel));
-	}
+    public Loyalty get(ServerPlayerEntity player) {
+        return this.data.get(player.getUuid());
+    }
 
-	public void addLoyaltyLevel(ServerPlayerEntity player, int loyaltyValue) {
-		int playerLevel = this.get(player).level;
-		int newLevel = Math.min(Math.max(playerLevel + loyaltyValue, 0), Loyalty.OWNER.level);
-		this.setLoyalty(player, this.levelToLoyaltyValue(newLevel));
-	}
+    public void set(ServerPlayerEntity player, Loyalty loyalty) {
+        this.data.put(player.getUuid(), loyalty);
+        this.sync();
+    }
 
-	public void setLoyalty(ServerPlayerEntity player, Loyalty loyalty) {
-		this.data().put(player.getUuid(), loyalty);
+    public void update(ServerPlayerEntity player, Function<Loyalty, Loyalty> consumer) {
+        Loyalty current = this.get(player);
+        current = consumer.apply(current);
 
-		this.sync();
-	}
+        this.set(player, current);
+    }
 
-	public Loyalty get(ServerPlayerEntity player) {
-		return this.data().get(player.getUuid());
-	}
+    public void subLevel(ServerPlayerEntity player, int level) {
+        this.update(player, loyalty -> loyalty.subtract(level));
+    }
 
-	public Loyalty levelToLoyaltyValue(int level) {
-		if (level > 0 && level <= 25) {
-			return Loyalty.NEUTRAL;
-		}
-		if (level > 25 && level <= 50) {
-			return Loyalty.COMPANION;
-		}
-		if (level > 50 && level <= 75) {
-			return Loyalty.PILOT;
-		}
-		if (level > 75 && level <= 100) {
-			return Loyalty.OWNER;
-		}
-		return Loyalty.REJECT;
-	}
+    public void addLevel(ServerPlayerEntity player, int level) {
+        this.update(player, loyalty -> loyalty.add(level));
+    }
 }
