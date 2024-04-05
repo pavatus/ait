976ab6a38cf793eb96808dd1474346a7a611c70c
den <den@den.ru>
2024-04-05 17:51:23 +0300

    Re-named LoyaltyData to LoyaltyHandler (what even is the difference between the *Data and *Handler? whatever)

--- src/main/java/loqor/ait/tardis/data/loyalty/LoyaltyHandler.java
+++ src/main/java/loqor/ait/tardis/data/loyalty/LoyaltyHandler.java
@@ -2,6 +2,7 @@ package loqor.ait.tardis.data.loyalty;
 
 import loqor.ait.tardis.Tardis;
 import loqor.ait.tardis.data.TardisLink;
+import loqor.ait.tardis.data.properties.PropertiesHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 
 import java.util.HashMap;
@@ -9,15 +10,15 @@ import java.util.Map;
 import java.util.UUID;
 import java.util.function.Function;
 
-public class LoyaltyData extends TardisLink {
+public class LoyaltyHandler extends TardisLink {
     private final Map<UUID, Loyalty> data;
 
-    public LoyaltyData(Tardis tardis, HashMap<UUID, Loyalty> data) {
+    public LoyaltyHandler(Tardis tardis, HashMap<UUID, Loyalty> data) {
         super(tardis, "loyalty");
         this.data = data;
     }
 
-    public LoyaltyData(Tardis tardis) {
+    public LoyaltyHandler(Tardis tardis) {
         this(tardis, new HashMap<>());
     }
 
@@ -25,12 +26,8 @@ public class LoyaltyData extends TardisLink {
         return this.data;
     }
 
-    public void add(ServerPlayerEntity player) {
-        this.set(player, new Loyalty(Loyalty.Type.NEUTRAL));
-    }
-
     public Loyalty get(ServerPlayerEntity player) {
-        return this.data.get(player.getUuid());
+        return this.data.getOrDefault(player.getUuid(), new Loyalty(Loyalty.Type.NEUTRAL));
     }
 
     public void set(ServerPlayerEntity player, Loyalty loyalty) {
