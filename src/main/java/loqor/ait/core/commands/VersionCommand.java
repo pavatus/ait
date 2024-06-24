6702f7b6505a39ab9f0644fe7e1ee5433bab5026
Loqor <65447472+Loqor@users.noreply.github.com>
2024-06-24 15:12:43 -0500

    did ascii art because i like to torture myself <333

--- src/main/java/loqor/ait/core/commands/VersionCommand.java
+++ src/main/java/loqor/ait/core/commands/VersionCommand.java
@@ -6,9 +6,13 @@ import com.mojang.brigadier.context.CommandContext;
 import loqor.ait.AITMod;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.ModContainer;
+import net.minecraft.client.font.FontLoader;
+import net.minecraft.client.font.FontManager;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
+import net.minecraft.util.Identifier;
 
 import static net.minecraft.server.command.CommandManager.literal;
 
@@ -17,11 +21,12 @@ public class VersionCommand {
     private static final ModContainer AIT = FabricLoader.getInstance().getModContainer(AITMod.MOD_ID).get();
 
     private static final String LOGO = """
-                \\\\    _ _| __ __|
-               _ \\\\     |     | \s
-              ___ \\\\    |     | \s
-            _/    _\\\\ ___|   _| \s
-                                """;
+           ::::::\\\\     ::::::::::::|| ::::::::::::::::||
+          == ==\\\\      ==||      ==||
+         =======\\\\     ==||      ==||
+        ##//   ##\\\\    ##||      ##||
+       ##//     ##\\\\ ######||    ##||
+            """;
 
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
         dispatcher.register(literal(AITMod.MOD_ID)
@@ -34,7 +39,7 @@ public class VersionCommand {
     private static int run(CommandContext<ServerCommandSource> context) {
         ServerCommandSource source = context.getSource();
 
-        source.sendMessage(Text.literal(LOGO).copy().formatted(Formatting.GOLD));
+        source.sendMessage(Text.literal(LOGO).copy().setStyle(Style.EMPTY.withFont(new Identifier("uniform"))).formatted(Formatting.GOLD));
         source.sendMessage(Text.translatable("message.ait.version", AIT.getMetadata().getVersion().getFriendlyString()));
 
         return Command.SINGLE_SUCCESS;
