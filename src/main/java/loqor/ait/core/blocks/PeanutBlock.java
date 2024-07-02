b32c6fb96282e2bbc12c1ee16ad3bfcc1b8328b8
DrTheodor <DrTheodor@users.noreply.github.com>
2024-07-02 16:15:26 +0300

    make peanut 1000x less powerful

--- src/main/java/loqor/ait/core/blocks/PeanutBlock.java
+++ src/main/java/loqor/ait/core/blocks/PeanutBlock.java
@@ -11,8 +11,8 @@ public class PeanutBlock extends Block {
     }
 
     public void explode(World world, BlockPos pos) {
-        world.createExplosion(null, world.getDamageSources().outOfWorld(), null, pos.toCenterPos(), 1000, true, World.ExplosionSourceType.MOB);
-        world.createExplosion(null, world.getDamageSources().outOfWorld(), null, pos.toCenterPos(), 1000, true, World.ExplosionSourceType.BLOCK);
-        world.createExplosion(null, world.getDamageSources().outOfWorld(), null, pos.toCenterPos(), 1000, true, World.ExplosionSourceType.TNT);
+        world.createExplosion(null, world.getDamageSources().outOfWorld(), null, pos.toCenterPos(), 100, true, World.ExplosionSourceType.MOB);
+        world.createExplosion(null, world.getDamageSources().outOfWorld(), null, pos.toCenterPos(), 100, true, World.ExplosionSourceType.BLOCK);
+        world.createExplosion(null, world.getDamageSources().outOfWorld(), null, pos.toCenterPos(), 100, true, World.ExplosionSourceType.TNT);
     }
 }
