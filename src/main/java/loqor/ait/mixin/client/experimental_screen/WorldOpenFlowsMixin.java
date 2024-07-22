68aa112eb8eeb63ec415485e425240651afe6fff
Dr Theo <71725814+DrTheodor@users.noreply.github.com>
2024-07-22 21:56:15 +0700

    pffft who needs that anyway

--- src/main/java/loqor/ait/mixin/client/experimental_screen/WorldOpenFlowsMixin.java
+++ src/main/java/loqor/ait/mixin/client/experimental_screen/WorldOpenFlowsMixin.java
@@ -8,19 +8,10 @@ import net.minecraft.server.integrated.IntegratedServerLoader;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = IntegratedServerLoader.class)
 public class WorldOpenFlowsMixin {
-
-	@ModifyVariable(method = "start(Lnet/minecraft/client/gui/screen/Screen;Ljava/lang/String;ZZ)V", at = @At("STORE"), ordinal = 3)
-	public boolean loadLevel_bl2(boolean canShowBackupPrompt) {
-		if (!AITMod.AIT_CONFIG.SHOW_EXPERIMENTAL_WARNING())
-			return false;
-
-		return canShowBackupPrompt;
-	}
 
 	@Inject(method = "tryLoad", at = @At(value = "INVOKE_ASSIGN", target = "Lcom/mojang/serialization/Lifecycle;experimental()Lcom/mojang/serialization/Lifecycle;", remap = false), cancellable = true)
 	private static void confirmWorldCreation(MinecraftClient client, CreateWorldScreen parent, Lifecycle lifecycle, Runnable loader, boolean bypassWarnings, CallbackInfo ci) {
