77a4d0d8a6d4db60137f75bdb7c60e38821f6712
Loqor <65447472+Loqor@users.noreply.github.com>
2024-08-07 02:29:42 -0500

    added a mixin so you can check your loyalty when you sleep in a bed in the tardis

---
+++ src/main/java/loqor/ait/mixin/server/BedInTardisMixin.java
@@ -0,0 +1,34 @@
+package loqor.ait.mixin.server;
+
+import loqor.ait.client.util.ClientTardisUtil;
+import loqor.ait.tardis.Tardis;
+import loqor.ait.tardis.data.loyalty.LoyaltyHandler;
+import loqor.ait.tardis.util.TardisUtil;
+import net.minecraft.block.BedBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BedBlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(BedBlock.class)
+public class BedInTardisMixin {
+    @Inject(at = @At("TAIL"), method = "onUse")
+    private void ait$useOn(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<ActionResult> cir) {
+        if (world.isClient()) {
+            Tardis tardis = ClientTardisUtil.getCurrentTardis();
+            if (tardis != null) {
+                player.sendMessage(Text.translatable("message.ait.loyalty_amount", tardis.loyalty().get(player)));
+            }
+        }
+    }
+
+}
