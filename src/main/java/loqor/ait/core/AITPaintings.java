45d0b26f46b4b52895022772386426a9bb3f44cb
DrTheodor <DrTheodor@users.noreply.github.com>
2024-08-21 12:38:04 +0300

    feat: crab thrower

---
+++ src/main/java/loqor/ait/core/AITPaintings.java
@@ -0,0 +1,10 @@
+package loqor.ait.core;
+
+import net.minecraft.entity.decoration.painting.PaintingVariant;
+
+import loqor.ait.core.util.PaintingRegistryContainer;
+
+public class AITPaintings implements PaintingRegistryContainer {
+
+    public static final PaintingVariant CRAB_THROWER = new PaintingVariant(48, 32);
+}
