d8357c7380fd6e9c3d891d4de9c1ba6c230082cd
James Hall <james.hall01@outlook.com>
2024-08-22 21:00:50 +0100

    Block Control (#366) fix

--- src/main/java/loqor/ait/core/util/ServerLifecycleHooks.java
+++ src/main/java/loqor/ait/core/util/ServerLifecycleHooks.java
@@ -2,18 +2,19 @@ package loqor.ait.core.util;
 
 
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+
 import net.minecraft.server.MinecraftServer;
 
 public class ServerLifecycleHooks {
 
-	private static MinecraftServer server;
+    private static MinecraftServer server;
 
-	public static void init() {
-		ServerLifecycleEvents.SERVER_STARTED.register(server -> ServerLifecycleHooks.server = server);
-		ServerLifecycleEvents.SERVER_STOPPING.register(server -> ServerLifecycleHooks.server = null);
-	}
+    public static void init() {
+        ServerLifecycleEvents.SERVER_STARTED.register(server -> ServerLifecycleHooks.server = server);
+        ServerLifecycleEvents.SERVER_STOPPING.register(server -> ServerLifecycleHooks.server = null);
+    }
 
-	public static MinecraftServer get() {
-		return server;
-	}
+    public static MinecraftServer get() {
+        return server;
+    }
 }
