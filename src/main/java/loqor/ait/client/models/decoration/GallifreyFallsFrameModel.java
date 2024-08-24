448262babeec42ebd95fd01ffdf0f1155e82d92d
Loqor <65447472+Loqor@users.noreply.github.com>
2024-08-23 20:05:48 -0500

    RAHHHHHHH I HATE RENDERINGGGGGGGGGG

--- src/main/java/loqor/ait/client/models/decoration/GallifreyFallsFrameModel.java
+++ src/main/java/loqor/ait/client/models/decoration/GallifreyFallsFrameModel.java
@@ -3,11 +3,16 @@ package loqor.ait.client.models.decoration;// Made with Blockbench 4.10.4
 // Paste this class into your mod and generate all required imports
 
 
+import net.minecraft.client.gl.Framebuffer;
 import net.minecraft.client.model.*;
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.model.SinglePartEntityModel;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
+
+import loqor.ait.client.renderers.entities.GallifreyFallsPaintingEntityRenderer;
 
 public class GallifreyFallsFrameModel extends SinglePartEntityModel {
     private final ModelPart frame;
@@ -24,7 +29,13 @@ public class GallifreyFallsFrameModel extends SinglePartEntityModel {
     }
     @Override
     public void render(MatrixStack matrices, VertexConsumer vertexConsumer, int light, int overlay, float red, float green, float blue, float alpha) {
+        frame.getChild("plane").visible = false;
         frame.render(matrices, vertexConsumer, light, overlay, red, green, blue, alpha);
+    }
+
+    public void renderWithFbo(MatrixStack matrices, VertexConsumerProvider vertexConsumer, Framebuffer buffer, int light, int overlay, float red, float green, float blue, float alpha) {
+        //this.render(matrices, vertexConsumer, light, overlay, red, green, blue, alpha);
+        frame.getChild("plane").render(matrices, vertexConsumer.getBuffer(RenderLayer.getEntityCutout(GallifreyFallsPaintingEntityRenderer.FRAME_TEXTURE)), light, overlay, red, green, blue, alpha);
     }
 
     @Override
