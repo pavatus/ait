967ba125652c6af2b7158b3babd43ada75c2183e
Loqor <65447472+Loqor@users.noreply.github.com>
2024-09-15 16:21:07 -0500

    im a little silly

--- src/main/java/loqor/ait/core/util/PlanetInformation.java
+++ src/main/java/loqor/ait/core/util/PlanetInformation.java
@@ -5,5 +5,5 @@ import net.minecraft.world.World;
 // @TODO: use for the new dimensions being added - Loqor
 public interface PlanetInformation {
     boolean ait$hasOxygen(World world);
-    float ait$getGravity(float gravity);
+    float ait$getGravity();
 }
