3e879d1943b0d217e31698cb3ad979151545c68f
Loqor <65447472+Loqor@users.noreply.github.com>
2024-09-15 16:01:40 -0500

    add new interface for adding information to planetary dimensions

---
+++ src/main/java/loqor/ait/core/util/PlanetInformation.java
@@ -0,0 +1,9 @@
+package loqor.ait.core.util;
+
+import net.minecraft.world.World;
+
+// @TODO: use for the new dimensions being added - Loqor
+public interface PlanetInformation {
+    boolean ait$hasOxygen(World world);
+    float ait$getGravity(float gravity);
+}
