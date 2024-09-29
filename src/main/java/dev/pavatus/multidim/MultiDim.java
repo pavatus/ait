4b84412bbc93f70b26da4c8ba22c2b9014a35457
DrTheodor <DrTheodor@users.noreply.github.com>
2024-09-29 21:28:02 +0300

    i think its done? @Loqor please test
    Signed-off-by: DrTheodor <DrTheodor@users.noreply.github.com>

--- src/main/java/dev/pavatus/multidim/MultiDim.java
+++ src/main/java/dev/pavatus/multidim/MultiDim.java
@@ -2,12 +2,15 @@ package dev.pavatus.multidim;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Set;
 
 import com.mojang.serialization.Lifecycle;
 import dev.pavatus.multidim.api.MultiDimServer;
 import dev.pavatus.multidim.api.MutableRegistry;
 import dev.pavatus.multidim.api.WorldBuilder;
 import dev.pavatus.multidim.impl.SimpleWorldProgressListener;
+import it.unimi.dsi.fastutil.objects.ReferenceOpenHashSet;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
 import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
@@ -16,7 +19,9 @@ import org.slf4j.LoggerFactory;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraft.world.dimension.DimensionOptions;
 import net.minecraft.world.level.storage.LevelStorage;
@@ -25,6 +30,10 @@ public class MultiDim  {
 
     private static MultiDim instance;
     private static final Logger LOGGER = LoggerFactory.getLogger("ait-multidim");
+
+    static {
+        ServerTickEvents.START_SERVER_TICK.register(server -> MultiDim.get(server).tick());
+    }
 
     public static MultiDim get(MinecraftServer server) {
         if (instance == null || instance.server != server)
@@ -35,11 +44,14 @@ public class MultiDim  {
 
     private final MinecraftServer server;
 
+    private final Set<ServerWorld> toDelete = new ReferenceOpenHashSet<>();
+    private final Set<ServerWorld> toUnload = new ReferenceOpenHashSet<>();
+
     private MultiDim(MinecraftServer server) {
         this.server = server;
     }
 
-    @SuppressWarnings({"resource"})
+    @SuppressWarnings("resource")
     public void remove(RegistryKey<World> key) {
         ServerWorld world = ((MultiDimServer) this.server).multidim$removeWorld(key);
 
@@ -74,15 +86,17 @@ public class MultiDim  {
         if (wasFrozen)
             dimensionsRegistry.multidim$unfreeze();
 
-        RegistryKey<DimensionOptions> key = RegistryKey.of(RegistryKeys.DIMENSION, builder.id());
+        DimensionOptions options = builder.buildOptions(this.server);
+        RegistryKey<DimensionOptions> key = RegistryKey.of(RegistryKeys.DIMENSION, options.dimensionTypeEntry()
+                .getKey().map(RegistryKey::getValue).orElse(builder.id()));
 
         if (!dimensionsRegistry.contains(key))
-            dimensionsRegistry.add(key, builder.buildOptions(), Lifecycle.stable());
+            dimensionsRegistry.add(key, options, Lifecycle.stable());
 
         if (wasFrozen)
             dimensionsRegistry.multidim$freeze();
 
-        ServerWorld world = builder.build();
+        ServerWorld world = builder.build(this.server, options);
         ((MultiDimServer) this.server).multidim$addWorld(world);
 
         ServerWorldEvents.LOAD.invoker().onWorldLoad(this.server, world);
@@ -102,5 +116,66 @@ public class MultiDim  {
             ServerWorldEvents.UNLOAD.invoker().onWorldUnload(this.server, world);
             MultiDimUtil.getMutableDimensionsRegistry(this.server).multidim$remove(key.getValue());
         }), true, false);
+    }
+
+    public void removeLater(ServerWorld world) {
+        this.server.submit(() -> this.toDelete.add(world));
+    }
+
+    public void unloadLater(ServerWorld world) {
+        this.server.submit(() -> this.toUnload.add(world));
+    }
+
+    private void tick() {
+        Set<ServerWorld> deletionQueue = this.toDelete;
+
+        if (!deletionQueue.isEmpty())
+            deletionQueue.removeIf(this::tickDeleteWorld);
+
+        Set<ServerWorld> unloadingQueue = this.toUnload;
+
+        if (!unloadingQueue.isEmpty())
+            unloadingQueue.removeIf(this::tickUnloadWorld);
+    }
+
+    private boolean prepareForUnload(ServerWorld world) {
+        if (this.isWorldUnloaded(world)) {
+            return true;
+        }
+
+        this.kickPlayers(world);
+        return false;
+    }
+
+    private boolean tickDeleteWorld(ServerWorld world) {
+        if (!this.prepareForUnload(world))
+            return false;
+
+        this.remove(world.getRegistryKey());
+        return true;
+    }
+
+    private boolean tickUnloadWorld(ServerWorld world) {
+        if (!this.prepareForUnload(world))
+            return false;
+
+        this.unload(world.getRegistryKey());
+        return true;
+    }
+
+    public boolean isWorldUnloaded(ServerWorld world) {
+        return world.getPlayers().isEmpty() && world.getChunkManager().getLoadedChunkCount() <= 0;
+    }
+
+    public void kickPlayers(ServerWorld world) {
+        if (world.getPlayers().isEmpty())
+            return;
+
+        ServerWorld overworld = this.server.getOverworld();
+        Vec3d spawnPos = overworld.getSpawnPos().toCenterPos();
+
+        for (ServerPlayerEntity player : world.getPlayers()) {
+            player.teleport(overworld, spawnPos.getX(), spawnPos.getY(), spawnPos.getZ(), player.getYaw(), player.getPitch());
+        }
     }
 }
--- src/main/java/dev/pavatus/multidim/impl/SimpleWorldProgressListener.java
+++ src/main/java/dev/pavatus/multidim/impl/SimpleWorldProgressListener.java
@@ -1,27 +1,12 @@
 package dev.pavatus.multidim.impl;
 
-import net.minecraft.text.Text;
-import net.minecraft.util.ProgressListener;
-
-public class SimpleWorldProgressListener implements ProgressListener {
+public class SimpleWorldProgressListener extends AbstractWorldProgressListener {
 
     private final Runnable onDone;
 
     public SimpleWorldProgressListener(Runnable onDone) {
         this.onDone = onDone;
     }
-
-    @Override
-    public void setTitle(Text title) { }
-
-    @Override
-    public void setTitleAndTask(Text title) { }
-
-    @Override
-    public void setTask(Text task) { }
-
-    @Override
-    public void progressStagePercentage(int percentage) { }
 
     @Override
     public void setDone() {
