6e0d6b9826ed08f26b7eddb956fbff29e7e61965
Loqor <65447472+Loqor@users.noreply.github.com>
2024-10-29 02:52:47 -0500

    not entirely sure why the respirator has decided to throw a fit but someone should take a look at it soon, i need sleep

--- src/main/java/loqor/ait/mixin/client/rendering/HeadFeatureRendererMixin.java
+++ src/main/java/loqor/ait/mixin/client/rendering/HeadFeatureRendererMixin.java
@@ -23,7 +23,7 @@ public abstract class HeadFeatureRendererMixin<T extends LivingEntity, M extends
         super(context);
     }
 
-    @Inject(method = "render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/entity/LivingEntity;FFFFFF)V", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/entity/LivingEntity;FFFFFF)V", at = @At("TAIL"), cancellable = true)
     public void ait$render(MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i,
             T livingEntity, float f, float g, float h, float j, float k, float l, CallbackInfo ci) {
         if (livingEntity.getEquippedStack(EquipmentSlot.HEAD).getItem() instanceof RenderableArmorItem armor && armor.hasCustomRendering()) {
