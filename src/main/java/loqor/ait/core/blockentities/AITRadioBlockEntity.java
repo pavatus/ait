4083713ff061f8dede855bd91b310dbf3b195af1
James Hall <james.hall01@outlook.com>
2024-12-10 15:04:35 +0000

    chore: compress MANY sounds + textures

--- src/main/java/loqor/ait/core/blockentities/AITRadioBlockEntity.java
+++ src/main/java/loqor/ait/core/blockentities/AITRadioBlockEntity.java
@@ -22,7 +22,6 @@ import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.World;
 
 import loqor.ait.core.AITBlockEntityTypes;
-import loqor.ait.core.AITSounds;
 
 public class AITRadioBlockEntity extends BlockEntity {
 
@@ -131,7 +130,7 @@ public class AITRadioBlockEntity extends BlockEntity {
         // Loqor
         // @Creativious
         if (((int) (Math.nextUp(this.getTickRot("volume") * (180 / Math.PI) * 11) / 360) + 1) == 10) {
-            world.playSound(null, pos, AITSounds.SECRET_MUSIC, SoundCategory.MASTER, 1F, 1F);
+            // world.playSound(null, pos, AITSounds.SECRET_MUSIC, SoundCategory.MASTER, 1F, 1F);
         }
 
         if (this.isRadioOn()) {
