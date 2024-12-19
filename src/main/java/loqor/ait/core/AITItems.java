caab3182013cf446a4d54f64d302f4e74e325ede
Maketendo <najim.oria59@hotmail.com>
2024-12-19 22:04:14 +0000

    stuff

--- src/main/java/loqor/ait/core/AITItems.java
+++ src/main/java/loqor/ait/core/AITItems.java
@@ -3,6 +3,7 @@ package loqor.ait.core;
 import java.util.ArrayList;
 import java.util.List;
 
+import io.wispforest.owo.itemgroup.OwoItemGroup;
 import io.wispforest.owo.itemgroup.OwoItemSettings;
 import io.wispforest.owo.registration.reflect.AutoRegistryContainer;
 import io.wispforest.owo.registration.reflect.ItemRegistryContainer;
@@ -15,7 +16,7 @@ import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.*;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
-import net.minecraft.item.MusicDiscItem;
+import net.minecraft.item.equipment.ArmorMaterials;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Rarity;
@@ -33,7 +34,7 @@ import loqor.ait.core.item.part.MachinePartItem;
 import loqor.ait.datagen.datagen_providers.util.NoEnglish;
 
 
-public class AITItems implements AutoRegistryContainer<BlockEntityType<?>> {
+public class AITItems {
     public static final FoodComponent ZEITON_DUST_FOOD = new FoodComponent.Builder().hunger(4).saturationModifier(0.3f)
             .statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 1000, 3), 1.0F)
             .statusEffect(new StatusEffectInstance(AITStatusEffects.ZEITON_HIGH, 500, 1), 1.0F)
@@ -43,21 +44,21 @@ public class AITItems implements AutoRegistryContainer<BlockEntityType<?>> {
     // TARDIS
     @NoEnglish
     public static final Item TARDIS_ITEM = new TardisItemBuilder(
-            new OwoItemSettings().group(AITMod.AIT_ITEM_GROUP).fireproof().maxCount(1));
+            new Item.Settings().group(AITMod.AIT_ITEM_GROUP).fireproof().maxCount(1));
     @NoEnglish
-    public static final Item SIEGE_ITEM = new SiegeTardisItem(new OwoItemSettings().fireproof());
+    public static final Item SIEGE_ITEM = new SiegeTardisItem(new Item.Settings().fireproof());
 
     // Functional Items
     @NoEnglish
     public static final Item REMOTE_ITEM = new RemoteItem(
-            new OwoItemSettings().group(AITMod.AIT_ITEM_GROUP).maxCount(1));
+            new Item.Settings().group(AITMod.AIT_ITEM_GROUP).maxCount(1));
     @NoEnglish
     public static final Item ARTRON_COLLECTOR = new ArtronCollectorItem(
-            new OwoItemSettings().group(AITMod.AIT_ITEM_GROUP).maxCount(1));
+            new Item.Settings().group(AITMod.AIT_ITEM_GROUP).maxCount(1));
     public static final Item RIFT_SCANNER = new RiftScannerItem(
-            new OwoItemSettings().group(AITMod.AIT_ITEM_GROUP).maxCount(1));
+            new Item.Settings().group(AITMod.AIT_ITEM_GROUP).maxCount(1));
     public static final Item HAMMER = new HammerItem(3, -2.4F,
-            new OwoItemSettings().group(AITMod.AIT_ITEM_GROUP).maxCount(1).maxDamage(600));
+            new Item.Settings().group(AITMod.AIT_ITEM_GROUP).maxCount(1).maxDamage(600));
     public static final Item RESPIRATOR = new RenderableArmorItem(ArmorMaterials.IRON, ArmorItem.Type.HELMET,
             new OwoItemSettings().group(AITMod.AIT_ITEM_GROUP).maxCount(1).maxDamage(80), true);;
     public static final Item FACELESS_RESPIRATOR = new RenderableArmorItem(ArmorMaterials.IRON,
@@ -69,104 +70,105 @@ public class AITItems implements AutoRegistryContainer<BlockEntityType<?>> {
     // Keys/Key Templates
     public static final Item IRON_KEY = new KeyItem(new OwoItemSettings().group(AITMod.AIT_ITEM_GROUP));
     public static final Item GOLD_KEY = new KeyItem(
-            new OwoItemSettings().rarity(Rarity.UNCOMMON).group(AITMod.AIT_ITEM_GROUP), KeyItem.Protocols.SNAP);
+            new Item.Settings().rarity(Rarity.UNCOMMON).group(AITMod.AIT_ITEM_GROUP), KeyItem.Protocols.SNAP);
     public static final Item NETHERITE_KEY = new KeyItem(
-            new OwoItemSettings().rarity(Rarity.RARE).group(AITMod.AIT_ITEM_GROUP).fireproof(), KeyItem.Protocols.SNAP,
+            new Item.Settings().rarity(Rarity.RARE).group(AITMod.AIT_ITEM_GROUP).fireproof(), KeyItem.Protocols.SNAP,
             KeyItem.Protocols.HAIL);
     public static final Item CLASSIC_KEY = new KeyItem(
-            new OwoItemSettings().rarity(Rarity.EPIC).group(AITMod.AIT_ITEM_GROUP), KeyItem.Protocols.SNAP,
+            new Item.Settings().rarity(Rarity.EPIC).group(AITMod.AIT_ITEM_GROUP), KeyItem.Protocols.SNAP,
             KeyItem.Protocols.HAIL);
 
     // Creative only skeleton key
     public static final Item SKELETON_KEY = new KeyItem(
-            new OwoItemSettings().rarity(Rarity.EPIC).group(AITMod.AIT_ITEM_GROUP), KeyItem.Protocols.SKELETON,
+            new Item.Settings().rarity(Rarity.EPIC).group(AITMod.AIT_ITEM_GROUP), KeyItem.Protocols.SKELETON,
             KeyItem.Protocols.HAIL, KeyItem.Protocols.SNAP);
     @NoEnglish
     public static final Item GOLD_KEY_UPGRADE_SMITHING_TEMPLATE = new KeySmithingTemplateItem(
-            new OwoItemSettings().group(AITMod.AIT_ITEM_GROUP), "Gold Key", "Gold Nugget");
+            new Item.Settings().group(AITMod.AIT_ITEM_GROUP), "Gold Key", "Gold Nugget");
     @NoEnglish
     public static final Item NETHERITE_KEY_UPGRADE_SMITHING_TEMPLATE = new KeySmithingTemplateItem(
-            new OwoItemSettings().group(AITMod.AIT_ITEM_GROUP), "Netherite Key", "Netherite Scrap");
+            new Item.Settings().group(AITMod.AIT_ITEM_GROUP), "Netherite Key", "Netherite Scrap");
     @NoEnglish
     public static final Item CLASSIC_KEY_UPGRADE_SMITHING_TEMPLATE = new KeySmithingTemplateItem(
-            new OwoItemSettings().group(AITMod.AIT_ITEM_GROUP), "Classic Key", "Amethyst Shard");
+            new Item.Settings().group(AITMod.AIT_ITEM_GROUP), "Classic Key", "Amethyst Shard");
     public static final Item SONIC_SCREWDRIVER = new SonicItem(new OwoItemSettings().group(AITMod.AIT_ITEM_GROUP));
 
     // Crafting items
     public static final Item ZEITON_SHARD = new ZeitonShardItem(new OwoItemSettings().group(AITMod.AIT_ITEM_GROUP));
     public static final Item CHARGED_ZEITON_CRYSTAL = new ChargedZeitonCrystalItem(
-            new OwoItemSettings().group(AITMod.AIT_ITEM_GROUP).maxCount(1));
+            new Item.Settings().group(AITMod.AIT_ITEM_GROUP).maxCount(1));
     public static final Item ZEITON_DUST = new Item(
-            new OwoItemSettings().group(AITMod.AIT_ITEM_GROUP).food(ZEITON_DUST_FOOD));
+            new Item.Settings().group(AITMod.AIT_ITEM_GROUP).food(ZEITON_DUST_FOOD));
 
     // Machine parts
     public static final Item ARTRON_FLUID_LINK = new FluidLinkItem(AbstractLinkItem.Type.ARTRON,
-            new OwoItemSettings() /* .group(AITMod.AIT_ITEM_GROUP) */);
+            new Item.Settings() /* .group(AITMod.AIT_ITEM_GROUP) */);
     public static final Item DATA_FLUID_LINK = new FluidLinkItem(AbstractLinkItem.Type.DATA,
-            new OwoItemSettings() /* .group(AITMod.AIT_ITEM_GROUP) */);
+            new Item.Settings() /* .group(AITMod.AIT_ITEM_GROUP) */);
     public static final Item VORTEX_FLUID_LINK = new FluidLinkItem(AbstractLinkItem.Type.VORTEX,
-            new OwoItemSettings() /* .group(AITMod.AIT_ITEM_GROUP) */);
+            new Item.Settings() /* .group(AITMod.AIT_ITEM_GROUP) */);
 
     public static final Item ARTRON_MERCURIAL_LINK = new MercurialLinkItem(AbstractLinkItem.Type.ARTRON,
-            new OwoItemSettings() /* .group(AITMod.AIT_ITEM_GROUP) */);
+            new Item.Settings() /* .group(AITMod.AIT_ITEM_GROUP) */);
     public static final Item DATA_MERCURIAL_LINK = new MercurialLinkItem(AbstractLinkItem.Type.DATA,
-            new OwoItemSettings() /* .group(AITMod.AIT_ITEM_GROUP) */);
+            new Item.Settings() /* .group(AITMod.AIT_ITEM_GROUP) */);
     public static final Item VORTEX_MERCURIAL_LINK = new MercurialLinkItem(AbstractLinkItem.Type.VORTEX,
-            new OwoItemSettings() /* .group(AITMod.AIT_ITEM_GROUP) */);
+            new Item.Settings() /* .group(AITMod.AIT_ITEM_GROUP) */);
 
     public static final Item CONDENSER = new MachinePartItem(MachinePartItem.Type.CONDENSER,
-            new OwoItemSettings() /* .group(AITMod.AIT_ITEM_GROUP) */);
+            new Item.Settings() /* .group(AITMod.AIT_ITEM_GROUP) */);
     public static final Item MANIPULATOR = new MachinePartItem(MachinePartItem.Type.MANIPULATOR,
-            new OwoItemSettings() /* .group(AITMod.AIT_ITEM_GROUP) */);
+            new Item.Settings() /* .group(AITMod.AIT_ITEM_GROUP) */);
     public static final Item BULB = new MachinePartItem(MachinePartItem.Type.BULB,
-            new OwoItemSettings() /* .group(AITMod.AIT_ITEM_GROUP) */);
+            new Item.Settings() /* .group(AITMod.AIT_ITEM_GROUP) */);
     public static final Item INDUCTOR = new MachinePartItem(MachinePartItem.Type.INDUCTOR,
-            new OwoItemSettings() /* .group(AITMod.AIT_ITEM_GROUP) */);
+            new Item.Settings() /* .group(AITMod.AIT_ITEM_GROUP) */);
 
     // Components
     public static final Item DEMATERIALIZATION_CIRCUIT = new SubSystemItem(
-            new OwoItemSettings().group(AITMod.AIT_ITEM_GROUP), SubSystem.Id.DEMAT);
+            new Item.Settings().group(AITMod.AIT_ITEM_GROUP), SubSystem.Id.DEMAT);
 
     public static final Item SHIELDS_CIRCUIT = new SubSystemItem(
-            new OwoItemSettings().group(AITMod.AIT_ITEM_GROUP), SubSystem.Id.SHIELDS);
+            new Item.Settings().group(AITMod.AIT_ITEM_GROUP), SubSystem.Id.SHIELDS);
     public static final Item BACKUP_CIRCUIT = new SubSystemItem(
-            new OwoItemSettings().group(AITMod.AIT_ITEM_GROUP), SubSystem.Id.EMERGENCY_POWER);
+            new Item.Settings().group(AITMod.AIT_ITEM_GROUP), SubSystem.Id.EMERGENCY_POWER);
     public static final Item GRAVITATIONAL_CIRCUIT = new SubSystemItem(
-            new OwoItemSettings().group(AITMod.AIT_ITEM_GROUP), SubSystem.Id.GRAVITATIONAL);
+            new Item.Settings().group(AITMod.AIT_ITEM_GROUP), SubSystem.Id.GRAVITATIONAL);
     public static final Item CHAMELEON_CIRCUIT = new SubSystemItem(
-            new OwoItemSettings().group(AITMod.AIT_ITEM_GROUP), SubSystem.Id.CHAMELEON);
+            new Item.Settings().group(AITMod.AIT_ITEM_GROUP), SubSystem.Id.CHAMELEON);
     public static final Item DESPERATION_CIRCUIT = new SubSystemItem(
-            new OwoItemSettings().group(AITMod.AIT_ITEM_GROUP), SubSystem.Id.DESPERATION);
+            new Item.Settings().group(AITMod.AIT_ITEM_GROUP), SubSystem.Id.DESPERATION);
     public static final Item STABILISERS = new SubSystemItem(
-            new OwoItemSettings().group(AITMod.AIT_ITEM_GROUP), SubSystem.Id.STABILISERS);
+            new Item.Settings().group(AITMod.AIT_ITEM_GROUP), SubSystem.Id.STABILISERS);
     public static final Item LIFE_SUPPORT = new SubSystemItem(
-            new OwoItemSettings().group(AITMod.AIT_ITEM_GROUP), SubSystem.Id.LIFE_SUPPORT);
+            new Item.Settings().group(AITMod.AIT_ITEM_GROUP), SubSystem.Id.LIFE_SUPPORT);
 
 
 
     // Blueprint
     public static final Item BLUEPRINT = new BlueprintItem(
-            new OwoItemSettings() /* .group(AITMod.AIT_ITEM_GROUP) */.rarity(Rarity.EPIC));
+            new new Item.Settings().rarity(Rarity.EPIC));
 
     // Waypoint-related
     public static final Item WAYPOINT_CARTRIDGE = new WaypointItem(
-            new OwoItemSettings().group(AITMod.AIT_ITEM_GROUP).maxCount(1));
+            new Item.Settings().group(AITMod.AIT_ITEM_GROUP).maxCount(1));
 
     // Music discs
     @NoEnglish
     public static final Item DRIFTING_MUSIC_DISC = new MusicDiscItem(1, AITSounds.DRIFTING_MUSIC,
-            new OwoItemSettings().maxCount(1).rarity(Rarity.RARE), 169);
+            new Item.Settings().maxCount(1).rarity(Rarity.RARE), 169);
 
     @NoEnglish
     public static final Item MERCURY_MUSIC_DISC = new MusicDiscItem(11, AITSounds.ERROR,
-            new OwoItemSettings().maxCount(1).rarity(Rarity.RARE), 216);
+            new Item.Settings().maxCount(1).rarity(Rarity.RARE), 216);
 
 
     // Block controls
 
      public static final Item REDSTONE_CONTROL = new
-             GenericControlBlockItem(AITBlocks.REDSTONE_CONTROL_BLOCK, new
-     OwoItemSettings().group(AITMod.AIT_ITEM_GROUP));
+             GenericControlBlockItem(AITBlocks.REDSTONE_CONTROL_BLOCK,
+     new Item.Settings().group(AITMod.AIT_ITEM_GROUP));
+
 
 
     public static List<Item> get() {
@@ -181,21 +183,4 @@ public class AITItems implements AutoRegistryContainer<BlockEntityType<?>> {
         return list;
     }
 
-    static {
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(entries -> {
-            entries.addAfter(Items.MUSIC_DISC_RELIC, DRIFTING_MUSIC_DISC);
-            entries.addAfter(DRIFTING_MUSIC_DISC, MERCURY_MUSIC_DISC);
-        });
-    }
-
-    @Override
-    public Registry<BlockEntityType<?>> getRegistry() {
-        return Registry.BLOCK_ENTITY_TYPE;
-    }
-
-
-    @Override
-    public Class<BlockEntityType<?>> getTargetFieldType() {
-        return null;
-    }
 }
