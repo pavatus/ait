0e8cd59b4e74336933f95d5015800997a7567e5f
Maketendo <najim.oria59@hotmail.com>
2024-12-21 10:21:48 +0000

    fix: game crash of a schema issue

--- src/main/java/loqor/ait/data/schema/exterior/category/JakeCategory.java
+++ src/main/java/loqor/ait/data/schema/exterior/category/JakeCategory.java
@@ -4,8 +4,6 @@ import net.minecraft.util.Identifier;
 
 import loqor.ait.AITMod;
 import loqor.ait.data.schema.exterior.ExteriorCategorySchema;
-import loqor.ait.data.schema.exterior.ExteriorVariantSchema;
-import loqor.ait.registry.impl.exterior.ExteriorVariantRegistry;
 
 public class JakeCategory extends ExteriorCategorySchema {
     public static final Identifier REFERENCE = new Identifier(AITMod.MOD_ID, "exterior/jake");
@@ -18,9 +16,11 @@ public class JakeCategory extends ExteriorCategorySchema {
     public boolean hasPortals() {
         return false;
     }
-
+/*
     @Override
     public ExteriorVariantSchema getDefaultVariant() {
         return ExteriorVariantRegistry.JAKE_DEFAULT;
     }
+
+ */
 }
