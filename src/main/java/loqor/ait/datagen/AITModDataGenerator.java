f0a8f09b3a5a378d38bc5d65c12a69aeb39ddcc5
James Hall <james.hall01@outlook.com>
2024-12-28 13:34:38 +0000

    feat: bootstrap message closes #815

--- src/main/java/loqor/ait/datagen/AITModDataGenerator.java
+++ src/main/java/loqor/ait/datagen/AITModDataGenerator.java
@@ -648,6 +648,7 @@ AITModDataGenerator implements DataGeneratorEntrypoint {
         provider.addTranslation("tardis.message.interiorchange.success", "%s has grown to %d");
         provider.addTranslation("tardis.message.landingpad.adjust", "Adjusting to landing pad..");
         provider.addTranslation("tardis.message.self_destruct.warning", "SELF DESTRUCT INITIATED | ABORT SHIP");
+        provider.addTranslation("tardis.message.growth.hint", "Shock the TARDIS' water supply with a charged crystal to bootstrap");
         provider.addTranslation("message.ait.control.ylandtype", "Vertical Search Mode: %s");
         provider.addTranslation("message.ait.loyalty_amount", "Loyalty Level: %s");
         provider.addTranslation("message.ait.landing_code", "Landing Code...");
