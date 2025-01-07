73b9334f1ab714861c7908625dca074fa47a15fc
Dr Theo <71725814+DrTheodor@users.noreply.github.com>
2025-01-07 23:08:10 +0300

    maybe fix door animations pt.2

--- src/main/java/loqor/ait/client/renderers/exteriors/ExteriorRenderer.java
+++ src/main/java/loqor/ait/client/renderers/exteriors/ExteriorRenderer.java
@@ -65,8 +65,6 @@ public class ExteriorRenderer<T extends ExteriorBlockEntity> implements BlockEnt
 
         Tardis tardis = optionalTardis.get();
 
-        profiler.swap("animate");
-        model.animateBlockEntity(entity, tardis.door().animationExteriorState().get());
 
         profiler.swap("render");
 
@@ -167,6 +165,9 @@ public class ExteriorRenderer<T extends ExteriorBlockEntity> implements BlockEnt
 
         if (tardis.selfDestruct().isQueued())
             matrices.scale(0.7f, 0.7f, 0.7f);
+
+        profiler.swap("animate");
+        model.animateBlockEntity(entity, tardis.door().animationExteriorState().get());
 
         model.renderWithAnimations(entity, this.model.getPart(), matrices,
                 vertexConsumers.getBuffer(AITRenderLayers.getEntityTranslucentCull(texture)), light, overlay, 1, 1, 1,
