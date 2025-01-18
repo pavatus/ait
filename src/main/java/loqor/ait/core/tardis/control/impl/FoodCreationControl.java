0e0021005d8cd1ecfc20cc7255215719a3a2403f
Saturn <60363818+Saturnorsomthing@users.noreply.github.com>
2025-01-18 20:57:55 +0100

    feat: renaissance can now make coffee! (removes 1500 fuel)

--- src/main/java/loqor/ait/core/tardis/control/impl/FoodCreationControl.java
+++ src/main/java/loqor/ait/core/tardis/control/impl/FoodCreationControl.java
@@ -1,10 +1,16 @@
 package loqor.ait.core.tardis.control.impl;
 
+import dev.drtheo.scheduler.api.Scheduler;
+import dev.drtheo.scheduler.api.TimeUnit;
+
+import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.text.Text;
+import net.minecraft.sound.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 
+import loqor.ait.core.AITItems;
+import loqor.ait.core.AITSounds;
 import loqor.ait.core.tardis.Tardis;
 import loqor.ait.core.tardis.control.Control;
 
@@ -17,8 +23,22 @@ public class FoodCreationControl extends Control {
     @Override
     public boolean runServer(Tardis tardis, ServerPlayerEntity player, ServerWorld world, BlockPos console,
                              boolean leftClick) {
-        player.sendMessage(Text.literal("Ya wana coppa tea??"));
+        if (player == null || world.isClient()) {
+            return false;
+        }
+        world.playSound(null, console, AITSounds.COFFEE_MACHINE, SoundCategory.BLOCKS, 1.0f, 1.0f);
+
+
+        Scheduler.get().runTaskLater(() -> {
+            if (player.isAlive() && player.getServerWorld().equals(world)) {
+                ItemStack coffeeItem = new ItemStack(AITItems.COFFEE);
+                tardis.removeFuel(1500);
+                if (!player.getInventory().insertStack(coffeeItem)) {
+                    player.dropItem(coffeeItem, false);
+                }
+            }
+        }, TimeUnit.TICKS, 180);
+
         return true;
     }
-
 }
