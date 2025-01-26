7d24bb3bdc395a5032c04d5a2454c17f7a839a80
Saturn <60363818+Saturnorsomthing@users.noreply.github.com>
2025-01-26 10:09:20 +0100

    fix: made it so the crystalline varients have smoke

--- src/main/java/loqor/ait/core/blocks/ConsoleBlock.java
+++ src/main/java/loqor/ait/core/blocks/ConsoleBlock.java
@@ -37,7 +37,9 @@ import loqor.ait.core.AITSounds;
 import loqor.ait.core.blockentities.ConsoleBlockEntity;
 import loqor.ait.core.blocks.types.HorizontalDirectionalBlock;
 import loqor.ait.core.item.HammerItem;
+import loqor.ait.data.schema.console.variant.crystalline.CrystallineMasterVariant;
 import loqor.ait.data.schema.console.variant.crystalline.CrystallineVariant;
+import loqor.ait.data.schema.console.variant.crystalline.CrystallineZeitonVariant;
 
 public class ConsoleBlock extends HorizontalDirectionalBlock implements BlockEntityProvider, ICantBreak {
 
@@ -171,27 +173,33 @@ public class ConsoleBlock extends HorizontalDirectionalBlock implements BlockEnt
     @Override
     public void randomDisplayTick(BlockState state, World world, BlockPos pos, net.minecraft.util.math.random.Random random) {
         BlockEntity blockEntity = world.getBlockEntity(pos);
+
         if (blockEntity instanceof ConsoleBlockEntity consoleBlockEntity) {
 
             if (consoleBlockEntity.tardis() == null) return;
 
             if (!consoleBlockEntity.tardis().get().fuel().hasPower()) return;
 
-            if (!(consoleBlockEntity.getVariant() instanceof CrystallineVariant)) return;
+            if (!(consoleBlockEntity.getVariant() instanceof CrystallineMasterVariant ||
+                    consoleBlockEntity.getVariant() instanceof CrystallineZeitonVariant ||
+                    consoleBlockEntity.getVariant() instanceof CrystallineVariant)) return;
 
             double d = pos.getX();
             double e = pos.getY();
             double f = pos.getZ();
+
             for (int i = 0; i < random.nextInt(15) + 1; ++i) {
                 boolean bl = random.nextBoolean();
                 float particleSpeed = random.nextFloat() / 15.0f;
+
                 world.addParticle(ParticleTypes.SMOKE,
-                        (double)pos.getX() + 0.5,
-                        (double)pos.getY() + 2,
-                        (double)pos.getZ() + 0.5,
+                        (double) pos.getX() + 0.5,
+                        (double) pos.getY() + 2,
+                        (double) pos.getZ() + 0.5,
                         bl ? particleSpeed : -particleSpeed,
                         2.2E-3,
                         bl ? particleSpeed : -particleSpeed);
+
                 world.addParticle(ParticleTypes.CLOUD,
                         pos.getX() + 0.5,
                         pos.getY() + 0.5,
@@ -202,6 +210,10 @@ public class ConsoleBlock extends HorizontalDirectionalBlock implements BlockEnt
             }
         }
     }
+
+
+
+
 
     @Override
     public void onTryBreak(World world, BlockPos pos, BlockState state) {
