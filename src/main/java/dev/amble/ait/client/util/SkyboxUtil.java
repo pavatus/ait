8be4adcac9d4a6e3ecab1ed5d7293fda62484ecf
Saturn <60363818+Saturnorsomthing@users.noreply.github.com>
2025-02-16 23:58:22 +0100

    feat: me

--- src/main/java/dev/amble/ait/client/util/SkyboxUtil.java
+++ src/main/java/dev/amble/ait/client/util/SkyboxUtil.java
@@ -33,6 +33,8 @@ public class SkyboxUtil extends WorldRenderer {
     private static final Identifier EARTH = AITMod.id("textures/environment/earth.png");
     private static final Identifier MOON = AITMod.id("textures/environment/moon.png");
     private static final Identifier MARS = AITMod.id("textures/environment/mars.png");
+    private static final Identifier SATURN = AITMod.id("textures/environment/saturn.png");
+    private static final Identifier SATURN_RING = AITMod.id("textures/environment/saturn_ring.png");
 
     public static final Quaternionf[] LOOKUP = new Quaternionf[]{null, RotationAxis.POSITIVE_X.rotationDegrees(90.0f),
             RotationAxis.POSITIVE_X.rotationDegrees(-90.0f), RotationAxis.POSITIVE_X.rotationDegrees(180.0f),
@@ -127,12 +129,13 @@ public class SkyboxUtil extends WorldRenderer {
         matrices.pop();
 
 
-
         // Planet Rendering
         renderStarBody(matrices, AITMod.id("textures/environment/tardis_star.png"), new Vec3d(31240, 1000, 0), new Vector3f(650f, 650f, 650f), new Vector2f(0, 0), true, new Vector3f(0.5f, 1, 1));
         renderCelestialBody(matrices, EARTH, new Vec3d(0, 0, 0), new Vector3f(900f, 900f, 900f), new Vector2f(0, 0), true, true, new Vector3f(0.18f, 0.35f, 0.60f));
         renderCelestialBody(matrices, MOON, new Vec3d(8240, 459f, 0), new Vector3f(150f, 150f, 150f), new Vector2f(0, 0), false, true, new Vector3f(0.5f, 0.5f, 0.5f));
         renderCelestialBody(matrices, MARS, new Vec3d(-2500, 1400, 10000), new Vector3f(500f, 500f, 500f), new Vector2f(0, 0), false, true, new Vector3f(0.5f, 1, 1));
+        renderCelestialBody(matrices, SATURN, new Vec3d(4500, 1400, 4500), new Vector3f(500f, 500f, 500f), new Vector2f(0, 0), false, false, new Vector3f(0.5f, 0, 1));
+        renderCelestialBody(matrices, SATURN_RING, new Vec3d(4500, 1200, 4500), new Vector3f(1000f, 20, 1000f), new Vector2f(0, 0), false, false, new Vector3f(0.5f, 1, 1));
     }
 
     /**
