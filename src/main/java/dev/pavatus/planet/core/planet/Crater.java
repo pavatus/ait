78cd0668f9fd0bcfec1c647b9728259ae56a3c65
Codiak <wallace@beansfamily.org>
2025-02-19 04:23:37 -0500

    Make the crater pretty Fix all 32 structures Make Mars pretty, 100% finished

--- src/main/java/dev/pavatus/planet/core/planet/Crater.java
+++ src/main/java/dev/pavatus/planet/core/planet/Crater.java
@@ -2,8 +2,6 @@ package dev.pavatus.planet.core.planet;
 
 import static loqor.ait.AITMod.MOD_ID;
 
-import java.util.Random;
-
 import com.mojang.serialization.Codec;
 
 import net.minecraft.block.Blocks;
@@ -28,26 +26,16 @@ public class Crater extends Feature<ProbabilityConfig> {
 
     @Override
     public boolean generate(FeatureContext<ProbabilityConfig> context) {
-
-        float rand = (float) new Random(System.currentTimeMillis()).nextInt(11) / 10;
-        if (rand < (context.getConfig().probability)) {
-            int radius = 5 + new Random(System.currentTimeMillis()).nextInt(10);
-            BlockPos pos = context.getWorld().getTopPosition(Heightmap.Type.WORLD_SURFACE_WG, context.getOrigin()).up(radius / 3);
-            pos.add(0, new Random(System.currentTimeMillis()).nextInt(4), 0);
-            //Hangs if ran on client-side
-            if (!context.getWorld().isClient()) {
-                for (BlockPos p : BlockPos.iterate(pos.add(-radius, -radius, -radius), pos.add(radius, radius, radius))) {
-                    if (p.isWithinDistance(pos, radius)) {
-                        if (!context.getWorld().getBlockState(p).isAir())
-                            context.getWorld().setBlockState(p, Blocks.AIR.getDefaultState(), 2);
-                    }
-                }
-                return true;
-            }
-            //Is client side? Return false
-            return false;
+        if (context.getWorld().isClient()) return false;
+        if (!((float) context.getWorld().getRandom().nextInt(11) / 10 < (context.getConfig().probability))) return false;
+        int radius = 5 + context.getWorld().getRandom().nextInt(10);
+        BlockPos pos = context.getWorld().getTopPosition(Heightmap.Type.WORLD_SURFACE_WG, context.getOrigin()).up(radius / 3);
+        pos.add(0, context.getWorld().getRandom().nextInt(4), 0);
+        for (BlockPos p : BlockPos.iterate(pos.add(-radius, -radius, -radius), pos.add(radius, radius, radius))) {
+            if (p.isWithinDistance(pos, radius))
+                if (!context.getWorld().getBlockState(p).isAir())
+                    context.getWorld().setBlockState(p, Blocks.AIR.getDefaultState(), 2);
         }
-        return false;
+        return true;
     }
-
 }
