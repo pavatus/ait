304037f0816c27e8a26dd088e0cd72613c3b6e40
Saturn <60363818+Saturnorsomthing@users.noreply.github.com>
2025-02-27 22:50:30 +0100

    chore: made it so if the tardis is in a overgrowth state ambient sounds can be heard in the nearby range

--- src/main/java/dev/amble/ait/core/tardis/handler/OvergrownHandler.java
+++ src/main/java/dev/amble/ait/core/tardis/handler/OvergrownHandler.java
@@ -1,18 +1,24 @@
 package dev.amble.ait.core.tardis.handler;
 
+import java.util.Random;
+
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ShearsItem;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
 
 import dev.amble.ait.api.KeyedTardisComponent;
 import dev.amble.ait.api.TardisEvents;
 import dev.amble.ait.api.TardisTickable;
+import dev.amble.ait.core.AITSounds;
 import dev.amble.ait.core.advancement.TardisCriterions;
+import dev.amble.ait.core.tardis.Tardis;
 import dev.amble.ait.core.tardis.handler.travel.TravelHandlerBase;
 import dev.amble.ait.data.Exclude;
 import dev.amble.ait.data.properties.bool.BoolProperty;
@@ -23,6 +29,7 @@ public class OvergrownHandler extends KeyedTardisComponent implements TardisTick
 
     private static final BoolProperty IS_OVERGROWN_PROPERTY = new BoolProperty("is_overgrown", false);
     private final BoolValue overgrown = IS_OVERGROWN_PROPERTY.create(this);
+    private static final Random RANDOM = new Random();
 
     @Exclude
     private static final int TIME_TO_OVERGROW = 24000;
@@ -30,6 +37,16 @@ public class OvergrownHandler extends KeyedTardisComponent implements TardisTick
     public static String TEXTURE_PATH = "textures/blockentities/exteriors/";
     private int ticks = 24000;
     private boolean ticking = false;
+    private int soundCooldown = 0;
+
+
+    private static final SoundEvent[] MOODY_SOUNDS = {
+            AITSounds.MOODY1,
+            AITSounds.MOODY2,
+            AITSounds.MOODY3,
+            AITSounds.MOODY4,
+            AITSounds.MOODY5
+    };
 
     static {
         TardisEvents.USE_DOOR.register((tardis, interior, world, player, pos) -> {
@@ -72,9 +89,11 @@ public class OvergrownHandler extends KeyedTardisComponent implements TardisTick
         this.setOvergrown(false);
         this.ticks = 0;
         this.ticking = false;
+        this.soundCooldown = 0;
     }
 
     @Environment(EnvType.CLIENT)
+    //Temp thing so the texture acctualy appears, loqor needs to datapack them
     public Identifier getOvergrownTexture() {
         ClientExteriorVariantSchema variant = tardis.getExterior().getVariant().getClient();
         Identifier baseTexture = variant.texture();
@@ -88,9 +107,12 @@ public class OvergrownHandler extends KeyedTardisComponent implements TardisTick
 
     @Override
     public void tick(MinecraftServer server) {
-        if (!tardis().fuel().hasPower()) {
+        Tardis tardis = this.tardis();
+
+        if (!tardis.fuel().hasPower()) {
             if (tardis.isGrowth() || this.isOvergrown()) {
                 this.ticking = false;
+                playMoodySounds(tardis);
                 return;
             }
 
@@ -117,6 +139,24 @@ public class OvergrownHandler extends KeyedTardisComponent implements TardisTick
             }
         } else {
             this.ticking = false;
+        }
+    }
+
+
+    private void playMoodySounds(Tardis tardis) {
+        if (!isOvergrown() || tardis == null)
+            return;
+
+        if (soundCooldown > 0) {
+            soundCooldown--;
+            return;
+        }
+
+        //idk why i did this tbh
+        if (RANDOM.nextFloat() < 0.005f) {
+            SoundEvent moodySound = MOODY_SOUNDS[RANDOM.nextInt(MOODY_SOUNDS.length)];
+            tardis.getExterior().playSound(moodySound, SoundCategory.AMBIENT, 0.15f, 1.0f);
+            soundCooldown = 400;
         }
     }
 }
