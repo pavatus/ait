b04ab12fcfb4ea19f7403f0920601f88e6fd5e90
Saturn <60363818+Saturnorsomthing@users.noreply.github.com>
2025-03-08 15:31:56 +0100

    chore: fixed some stuff, this needs optimizing real bad

--- src/main/java/dev/amble/ait/core/tardis/control/impl/ElectricalDischargeControl.java
+++ src/main/java/dev/amble/ait/core/tardis/control/impl/ElectricalDischargeControl.java
@@ -6,6 +6,7 @@ import dev.drtheo.scheduler.api.TimeUnit;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
@@ -22,12 +23,11 @@ import dev.amble.ait.core.tardis.control.impl.pos.IncrementManager;
 
 public class ElectricalDischargeControl extends Control {
 
-    //I wanted to make it so the higher the increment the higher the AU cost but it dident like that so
-    //Also i wanted to make it spawn particles but that stopped working for some reason and caused massive lag :Clueless:
     private static final int BASE_ARTRON_COST = 2750;
     private static final int BASE_EFFECT_RADIUS = 2;
     private static final int BASE_DURATION = 80;
     private static final int STATUS_EFFECT_DURATION = 400;
+    private static final int ARTRON_COST_PER_INCREMENT = 50;
 
     public ElectricalDischargeControl() {
         super("electrical_discharge");
@@ -40,16 +40,16 @@ public class ElectricalDischargeControl extends Control {
             return false;
         }
 
-
         int incrementLevel = IncrementManager.increment(tardis);
-        int effectRadius = BASE_EFFECT_RADIUS + (incrementLevel * 5);
+        int effectRadius = BASE_EFFECT_RADIUS + (incrementLevel + 5);
+        int artronCost = BASE_ARTRON_COST + (incrementLevel + ARTRON_COST_PER_INCREMENT);
 
-        if (tardis.fuel().getCurrentFuel() < BASE_ARTRON_COST) {
-            player.sendMessage(Text.literal("§cERROR: Insufficient Artron Energy!"), true);
+        if (tardis.fuel().getCurrentFuel() < artronCost) {
+            player.sendMessage(Text.literal("§cERROR: Insufficient Artron Energy! Required: " + artronCost + " AU"), true);
             return false;
         }
 
-        tardis.fuel().removeFuel(BASE_ARTRON_COST);
+        tardis.fuel().removeFuel(artronCost);
 
         BlockPos exteriorPos = tardis.travel().position().getPos();
         ServerWorld exteriorWorld = tardis.travel().position().getWorld();
@@ -59,6 +59,7 @@ public class ElectricalDischargeControl extends Control {
         Scheduler.get().runTaskLater(() -> {
             applyElectricalEffects(exteriorWorld, exteriorPos, effectRadius);
             exteriorWorld.playSound(null, exteriorPos, AITSounds.CLOISTER, SoundCategory.BLOCKS, 1.0F, 1.0F);
+            spawnElectricParticles(exteriorWorld, exteriorPos, effectRadius);
         }, TimeUnit.TICKS, 40);
 
         Scheduler.get().runTaskLater(() -> world.playSound(null, console, AITSounds.DING, SoundCategory.BLOCKS, 1.0F, 1.0F), TimeUnit.TICKS, BASE_DURATION);
@@ -73,6 +74,12 @@ public class ElectricalDischargeControl extends Control {
             entity.addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, STATUS_EFFECT_DURATION, 1));
             entity.addStatusEffect(new StatusEffectInstance(StatusEffects.POISON, STATUS_EFFECT_DURATION / 4, 1));
         });
+    }
+
+    private void spawnElectricParticles(ServerWorld world, BlockPos pos, int radius) {
+        for (BlockPos targetPos : BlockPos.iterate(pos.add(-radius, -1, -radius), pos.add(radius, 2, radius))) {
+            world.spawnParticles(ParticleTypes.ELECTRIC_SPARK, targetPos.getX() + 0.5, targetPos.getY() + 1.0, targetPos.getZ() + 0.5, 5, 0, 0.05, 0, 0.1);
+        }
     }
 
     @Override
